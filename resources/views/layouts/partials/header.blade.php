3f5e45d2b0d8beeb5d7210bbc5cb974c63f61fef
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-24 17:28:00 +0200

    [UPD]Created header without style

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header class="py-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC-Logo">
+                    </div>
+                    <div class="col-8">
+                        <ul class="list-unstyled fw-bold d-flex">
+                            <li class="px-2"><a href="#">CHARACTERS</a></li>
+                            <li class="px-2"><a href="#">COMICS</a></li>
+                            <li class="px-2"><a href="#">MOVIE</a></li>
+                            <li class="px-2"><a href="#">TV</a></li>
+                            <li class="px-2"><a href="#">GAMES</a></li>
+                            <li class="px-2"><a href="#">COLLECTIBLES</a></li>
+                            <li class="px-2"><a href="#">VIDEOS</a></li>
+                            <li class="px-2"><a href="#">FANS</a></li>
+                            <li class="px-2"><a href="#">NEWS</a></li>
+                            <li class="px-2"><a href="#">SHOP</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Search">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
+                          </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
