9e6bf67f748e3616722488f8f13f538d90a4589b
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-24 18:33:17 +0200

    [UPD]Created footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,80 @@
 <footer>
-  
+    <div class="bg-img">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 py-5">
+                    <div class="col">
+                        <div class="content">
+                            <h4>DC COMICS</h4>
+                            <ul class="text-secondary list-unstyled">
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                        </div>
+                        <div class="content">
+                            <h4>SHOP</h4>
+                            <ul class="text-secondary list-unstyled">
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content">
+                            <h4>DC</h4>
+                            <ul class="text-secondary list-unstyled">
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy(New)</li>
+                                <li>Ad Choices</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact us</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content">
+                            <h4>SITES</h4>
+                            <ul class="text-secondary list-unstyled">
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6"> 
+                    <img class="logo" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-footer p-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 d-flex justify-content-start align-self-center">
+                    <button><p>SIGN-UP NOW!</p></button>
+                </div>
+                <div class="col-6 d-flex justify-content-end align-self-center">
+                    <p class="fw-bold t-blue d-inline-block">FOLLOW US</p>
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}">
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
